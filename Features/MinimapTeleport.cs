46e54495f6f561a01df59d67dc744823837a6780
Liuhaixv <1244653065@qq.com>
2023-02-19 22:54:33 +0800

    bug

---
+++ Features/MinimapTeleport.cs
@@ -0,0 +1,111 @@
+﻿using Handlers.GameHandlers.PlayerHandlers;
+using Handlers.GameHandlers.SpecialBehaviour;
+using MelonLoader;
+using System;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.UI;
+using static MelonLoader.MelonLogger;
+
+namespace GGD_Hack.Features
+{
+    [RegisterTypeInIl2Cpp]
+    public class MinimapTeleport : MonoBehaviour
+    {
+        private static MinimapTeleport instance;
+        public MinimapTeleport(IntPtr ptr) : base(ptr) { }
+
+        // Optional, only used in case you want to instantiate this class in the mono-side
+        // Don't use this on MonoBehaviours / Components!
+        public MinimapTeleport() : base(ClassInjector.DerivedConstructorPointer<MinimapTeleport>()) => ClassInjector.DerivedConstructorBody(this);
+
+        public static void Init()
+        {
+            GameObject ML_Manager = GameObject.Find("ML_Manager");
+            if (ML_Manager == null)
+            {
+                ML_Manager = new GameObject("ML_Manager");
+                DontDestroyOnLoad(ML_Manager);
+            }
+
+            if (ML_Manager.GetComponent<MinimapTeleport>() == null)
+            {
+                instance = ML_Manager.AddComponent<MinimapTeleport>();
+            }
+        }
+
+        private void Teleport()
+        {
+
+        }
+
+        private Vector2? GetMouseLocalPositionOnMinimapPanel()
+        {
+            Vector2 localPoint;
+
+            try
+            {
+                GameObject panel = Utils.FindGameObjectByPath("Canvas/MiniMap/Panel");
+
+                if (panel == null) return null;
+
+                Image image = panel.GetComponent<Image>();
+
+                // 获取鼠标点击的屏幕坐标
+                Vector2 mousePos = UnityEngine.Input.mousePosition;
+
+                // 将屏幕坐标转换为相对于Image组件的坐标
+                RectTransform rectTransform = image.GetComponent<RectTransform>();
+
+                if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, mousePos, null, out localPoint))
+                {
+                    MelonLogger.Msg("Click position relative to MiniMap panel's image component: " + localPoint.x + ", " + localPoint.y);
+                    return localPoint;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Warning(ex.Message);
+                return null;
+            }
+        }
+
+        private Vector3? localPositionOnMinimapToPosition(Vector2 localPosition)
+        {
+            MiniMapHandler miniMapHandler = MinimapESP.miniMapHandler;
+
+            if (miniMapHandler == null) return null;
+
+            Vector3 vector3 = new Vector3(
+                (float)(localPosition.x - miniMapHandler.xOffset) / miniMapHandler.xFactor,
+                (float)(localPosition.y - miniMapHandler.yOffset) / miniMapHandler.yFactor,
+                0);
+
+            return vector3;
+        }
+
+        /// <summary>
+        /// 判断鼠标是否被点击并传送
+        /// </summary>
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2? mouseLocalPosition = GetMouseLocalPositionOnMinimapPanel();
+
+                if (!mouseLocalPosition.HasValue) return;
+
+                Vector3? targetTeleportPosition = localPositionOnMinimapToPosition(mouseLocalPosition.Value);
+
+                if (!targetTeleportPosition.HasValue) return;
+
+                //传送
+                LocalPlayer.Instance.transform.position = targetTeleportPosition.Value;
+            }
+        }
+    }
+}
--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using MelonLoader;
+using UnityEngine;
 
-namespace GGD_Hack.Utils
+namespace GGD_Hack
 {
     public class Utils
     {
@@ -25,6 +26,72 @@ namespace GGD_Hack.Utils
             {
                 MelonLogger.Error("ERROR!!: SendTextMessage");
             }
+        }
+
+        //获取LocalPlayer
+        public static Handlers.GameHandlers.PlayerHandlers.LocalPlayer GetLocalPlayer()
+        {
+            //通过tag查找玩家
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+
+            GameObject player = null;
+            foreach (GameObject gameObject in gameObjects)
+            {
+                if (gameObject.GetComponent<Handlers.GameHandlers.PlayerHandlers.LocalPlayer>() == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    player = gameObject;
+                    break;
+                }
+            }
+
+            //未找到玩家实例
+            if (player == null)
+            {
+                return null;
+            }
+
+            return player.GetComponent<Handlers.GameHandlers.PlayerHandlers.LocalPlayer>();
+        }
+
+        /// <summary>
+        /// 无论是否激活
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static GameObject FindGameObjectByPath(string path)
+        {
+            // 分割路径字符串
+            string[] pathElements = path.Split('/');
+
+            // 从根开始遍历场景层次结构
+            Transform currentTransform = null;
+            for (int i = 0; i < pathElements.Length; i++)
+            {
+                // 获取当前层级的Transform
+                if (currentTransform == null)
+                {
+                    // 如果是根节点
+                    currentTransform = GameObject.Find(pathElements[i]).transform;
+                }
+                else
+                {
+                    // 如果不是根节点
+                    currentTransform = currentTransform.Find(pathElements[i]);
+                }
+
+                // 如果找不到当前层级的Transform，返回 null
+                if (currentTransform == null)
+                {
+                    return null;
+                }
+            }
+
+            // 返回找到的 GameObject
+            return currentTransform.gameObject;
         }
     }
 }
