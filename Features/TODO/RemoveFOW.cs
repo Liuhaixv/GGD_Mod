99c14c9d7e2e493d774728d4bd54a8824c0045b9
Liuhaixv <49237108+Liuhaixv@users.noreply.github.com>
2023-03-24 22:18:03 +0800

    FOW

---
+++ Features/TODO/RemoveFOW.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO: 移除战争迷雾
+//Remove fog of war
+namespace GGD_Hack.Features
+{
+    public class RemoveFOW
+    {
+        public static MelonPreferences_Entry<bool> Enabled = MelonPreferences.CreateEntry<bool>("GGDH", "Enable_" + nameof(RemoveFOW), true);
+    }
+}
---
+++ Features/TODO/ZoomSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGD_Hack.Features
+{
+    public class ZoomSlider
+    {
+        //
+        private static void ChangeOrthographicSize(float newValue)
+        {
+
+        }
+    }
+}
